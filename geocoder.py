958021251a264f79d68c51ff69e7cc892e3d76d3
chelsea-str <111892341+chelsea-str@users.noreply.github.com>
2023-05-10 13:46:39 +0200

    Update geocoder.py

--- geocoder.py
+++ geocoder.py
@@ -1,50 +1,54 @@
 import requests
+import time
 
 
 def extract_cords(contact, address, name):
     var_dict = {'contact': contact, 'address': address, 'name': name}
-    place_api = 'YOUR_PLACE_API_KEY'
-    geocode_api = 'YOUR_GEOCODE_API_KEY'
+    key = 'YOUR_PLACE_API_KEY'
     base_url = "https://maps.googleapis.com/maps/api/"
-    place_api_url = f"place/textsearch/json?query={contact or name}&key={place_api}&region=ZA"
-    geocode_api_url = f"geocode/json?address={address}&key={geocode_api}&region=ZA"
+    place_url = f"place/textsearch/json?query={contact or name}&key={key}&region=ZA"
+    geocode_url = f"geocode/json?address={address}&key={key}&region=ZA"
     cords_list = []
 
     for item, value in var_dict.items():
-        if item == 'contact' or item == 'name':
-            url = base_url + place_api_url
-            geo = requests.get(url)
+        current_delay = 0.1
+        max_delay = 600
+        if item == 'contact' and item is not None or item == 'name':
+            url = base_url + place_url
         else:
-            url = base_url + geocode_api_url
-            geo = requests.get(url)
-        if geo.status_code not in range(200, 299):
-            cords_list.append('None')
-            cords_list.append('None')
-        else:
-            retry_count = 0
-            while retry_count < 3:
-                try:
-                    results = geo.json()['results'][0]
-                    lat = results['geometry']['location']['lat']
-                    lng = results['geometry']['location']['lng']
-                    if lat < -34.83 or lat > -22.12 or lng < 16.49 or lng > 32.89:
-                        cords_list.append('None')
-                        cords_list.append('None')
-                    else:
-                        cords_list.append(lat)
-                        cords_list.append(lng)
-                    break
-                except (KeyError, IndexError):
-                    cords_list.append('None')
-                    cords_list.append('None')
-                    break
-                except Exception as e:
-                    print(f"An error occurred: {e}")
-                    retry_count += 1
-                    if retry_count == 3:
-                        cords_list.append('None')
-                        cords_list.append('None')
+            url = base_url + geocode_url
+
+        while True:
+            try:
+                geo = requests.get(url)
+            except IOError:
+                pass
+            else:
+                if geo.status_code in range(200, 299):
+                    try:
+                        results = geo.json()['results'][0]
+                        lat = results['geometry']['location']['lat']
+                        lng = results['geometry']['location']['lng']
+                        if lat < -34.83 or lat > -22.12 or lng < 16.49 or lng > 32.89:
+                            cords_list.append('Wrong co-ordinates')
+                            cords_list.append('Wrong co-ordinates')
+                        else:
+                            cords_list.append(lat)
+                            cords_list.append(lng)
                         break
-                    else:
-                        continue
-    print(cords_list)
+                    except IndexError:
+                        pass
+                elif geo.status_code in range(200, 299):
+                    cords_list.append('Unsuccessful')
+                    cords_list.append('Unsuccessful')
+                    break
+
+            if current_delay > max_delay:
+                print('Taking too long')
+                cords_list.append('Took too long')
+                cords_list.append('Took too long')
+                break
+            print(f'Waiting {current_delay} seconds before retrying.')
+            time.sleep(current_delay)
+            current_delay *= 2
+    return cords_list
